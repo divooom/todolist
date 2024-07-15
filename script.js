9542c905e245bab8f938b19657c8752fccb9b7ac
divooom <ymotion9@gmail.com>
2024-07-15 19:11:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -259,6 +259,7 @@ document.addEventListener("DOMContentLoaded", () => {
             li.appendChild(deleteBtn);
         }
 
+        console.log('Created todo item:', { text, isDeleted }); //◆◇◆
         return li;
     }
 
